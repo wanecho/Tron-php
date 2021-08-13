51be6856b26f8f1ce7114de11ce57768f65f5cb2
wuheihei <229643466@qq.com>
2021-08-14 00:24:21 +0800

    gg

--- examples/custom-nodes.php
+++ examples/custom-nodes.php
@@ -7,11 +7,15 @@ use IEXBase\TronAPI\Tron;
 $fullNode = new HttpProvider('https://api.trongrid.io');
 $solidityNode = new HttpProvider('https://api.trongrid.io');
 $eventServer = new HttpProvider('https://api.trongrid.io');
-$privateKey = 'private_key';
 
 //Example 1
 try {
-    $tron = new Tron($fullNode, $solidityNode, $eventServer, $privateKey);
+    $tron = new Tron($fullNode, $solidityNode, $eventServer);
+    //$data= $tron->getBlockByNumber(32600826);
+   
+    $data=  $tron->getBlockRange(32760141,32760142);
+    var_dump($data);
+
 } catch (\IEXBase\TronAPI\Exception\TronException $e) {
     die($e->getMessage());
 }
